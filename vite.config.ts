9fbb71cf219dc3719a8bd8a8d40ab4de53dec912
dansagam <olukayodeogunnowo@yahoo.com>
2024-07-10 19:28:18 +0100

    fix: add the config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
 import dts from "vite-plugin-dts";
-import { resolve } from "path";
+import { extname, relative, resolve } from "path";
+import { fileURLToPath } from "node:url";
+import { glob } from "glob";
 import svgrPlugin from "vite-plugin-svgr";
-import { peerDependencies } from "./package.json";
+import { peerDependencies, name } from "./package.json";
 
 const umdGlobals = {
   react: "React",
@@ -13,37 +15,73 @@ const umdGlobals = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgrPlugin(), tsConfigPaths(), dts()],
+  plugins: [
+    react(),
+    svgrPlugin(),
+    tsConfigPaths(),
+    dts({
+      include: ["lib"],
+      // include: ["src/index.tsx"],
+      // beforeWriteFile: (filePath, content) => ({
+      //   filePath: filePath.replace("/src", ""),
+      //   content,
+      // }),
+    }),
+  ],
   resolve: {
-    alias: [{ find: "@/", replacement: resolve(__dirname, "./src/") }],
+    alias: [
+      { find: "@/", replacement: resolve(__dirname, "./src/") },
+      { find: "@library/", replacement: resolve(__dirname, "./lib/") },
+    ],
   },
+  // build: {
+  //   rollupOptions: {
+  //     input: "./src/index.tsx",
+  //     output: {
+  //       name: "ReactSpotflowCheckout",
+  //       exports: "named",
+  //       globals: umdGlobals,
+  //       format: "umd",
+  //     },
+  //     external: [...Object.keys(peerDependencies)],
+  //   },
+  //   lib: {
+  //     entry: "./src/index.ts", // Specifies the entry point for building the library.
+  //     name: "ReactSpotflowCheckout", // Sets the name of the generated library.
+  //     fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
+  //     formats: ["cjs", "es", "umd"], // Specifies the output formats (CommonJS and ES modules).
+  //   },
+  //   sourcemap: true,
+  //   emptyOutDir: true,
+  // },
   build: {
-    rollupOptions: {
-      input: "./src/index.tsx",
-      output: {
-        // manualChunks(id) {
-        //   if (id.includes(".svg?react")) {
-        //     return id
-        //       .toString()
-        //       .split("/")
-        //       [id.toString().split("/").length - 1].split("?react")[0]
-        //       .toString();
-        //   }
-        // },
-        name: "ReactSpotflowCheckout",
-        exports: "named",
-        globals: umdGlobals,
-        format: "umd",
-      },
-      external: [...Object.keys(peerDependencies)],
-    },
+    copyPublicDir: false,
     lib: {
-      entry: "./src/index.ts", // Specifies the entry point for building the library.
-      name: "react-spotflow-checkout", // Sets the name of the generated library.
-      fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
-      formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
     },
-    sourcemap: true,
-    emptyOutDir: true,
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)],
+      input: Object.fromEntries(
+        // https://rollupjs.org/configuration-options/#input
+        glob
+          .sync("lib/**/*.{ts,tsx}", {
+            ignore: ["lib/**/*.d.ts"],
+          })
+          .map((file) => [
+            // 1. The name of the entry point
+            // lib/nested/foo.js becomes nested/foo
+            relative("lib", file.slice(0, file.length - extname(file).length)),
+            // 2. The absolute path to the entry file
+            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
+    },
   },
+  assetsInclude: ["**/*.otf"],
 });
