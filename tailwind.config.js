f8c199177ead3dea7a6f7457867f65a7c35e41cf
dansagam <olukayodeogunnowo@yahoo.com>
2024-07-13 07:01:39 +0100

    feat: add the linear loader

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}", "./lib/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "progress-linear": {
+          "0%": { transform: "translateX(0)" },
+          "50%": { transform: "translateX(40%)" },
+          "100%": { transform: "translateX(100%)" },
+        },
+      },
+      animation: {
+        "progress-linear": "progress-linear 1s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
